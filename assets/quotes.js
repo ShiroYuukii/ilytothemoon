79256aad923f327ae3d628edf098dce68b572ec9
Azri <53987614+ShiroYuukii@users.noreply.github.com>
2024-06-02 19:03:16 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,7 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "azzzri", quotes: "stay with me, till the end" },
+    { author: "azzzri", quotes: "jangan lupa bermain robloxx yaa" },
+    { author: "azzzri", quotes: "hey! i wuvv chuuuu! UwU" },
+    { author: "azzzri", quotes: "i may not with you everyday, but i love you everyday." },
 ];
